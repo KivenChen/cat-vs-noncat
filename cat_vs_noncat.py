d96356c8df58f463ee8131246090ef3b40bbea4c
KivenChen <sdckivenchen@gmail.com>
2018-11-04 12:41:37 +0800

    init repo

--- cat_vs_noncat.py
+++ cat_vs_noncat.py
@@ -14,7 +14,7 @@ Let's first import all the packages that you will need during this assignment.
 # - [numpy](www.numpy.org) is the fundamental package for scientific computing with Python.
 # - [matplotlib](http://matplotlib.org) is a library to plot graphs in Python.
 # - [h5py](http://www.h5py.org) is a common package to interact with a datasets that is stored on an H5 file.
-# - [PIL](http://www.pythonware.com/products/pil/) and [scipy](https://www.scipy.org/) are used here to test your model with your own picture at the end.
+# - [PIL](http://www.pythonware.com/products/pil/) and [scipy](https://www.scipy.org/) are used here to test our model with your own picture at the end.
 # - dnn_app_utils provides the functions implemented in the "Building your Deep Neural Network: Step by Step" assignment to this notebook.
 # - np.random.seed(1) is used to keep all the random function calls consistent. It will help us grade your work.
 '''
